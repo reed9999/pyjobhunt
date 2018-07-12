03e3baa40b2c3d54827038815666bb2c63533523
reed9999 <reed9999@uw.edu>
2018-07-11 23:30:54 -0700

    Refactor to be a little OO

--- createresume.py
+++ createresume.py
@@ -1,40 +1,45 @@
 import uno
 
-def set_up():
-    global smgr
-    localContext = uno.getComponentContext()
-    resolver = localContext.ServiceManager.createInstanceWithContext(
-                    "com.sun.star.bridge.UnoUrlResolver", localContext )
-    ctx = connect_to_running_office(resolver)
-    smgr = ctx.ServiceManager
-    desktop = smgr.createInstanceWithContext( "com.sun.star.frame.Desktop",ctx)
-    model = get_current_writer_doc(desktop)
-    return {
-        'resolver': resolver,
-        'context': ctx,
-        'desktop': desktop,
-        'model': model,
-    }
+class ResumeCreator:
 
-def connect_to_running_office(the_resolver):
-    return the_resolver.resolve(
-        "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
+    def set_up(self):
+        global smgr
+        localContext = uno.getComponentContext()
+        self.resolver = localContext.ServiceManager.createInstanceWithContext(
+                        "com.sun.star.bridge.UnoUrlResolver", localContext )
+        self.context = self.connect_to_running_office()
+        self.service_manager = self.context.ServiceManager
+        self.desktop = self.service_manager.createInstanceWithContext(
+            "com.sun.star.frame.Desktop", self.context)
+        self.model = self.get_current_writer_doc()
 
+    def connect_to_running_office(self):
+        return self.resolver.resolve(
+            "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
 
+    def do_set_up(self):
+        self.set_up()
+        return {
+            'resolver': self.resolver,
+            'context': self.context,
+            'desktop': self.desktop,
+            'model': self.model,
+        }
 
-def get_current_writer_doc(desktop):
-    return desktop.getCurrentComponent()
+    def get_current_writer_doc(self):
+        self.current_writer_doc = self.desktop.getCurrentComponent()
+        return self.current_writer_doc
 
+    def create_new_file(self):
+        try:
+            # https://wiki.openoffice.org/wiki/Writer/API/Overview#Creating.2C_opening_a_Writer_Document
+            newdoc = self.desktop.loadComponentFromURL("private:factory/swriter",
+                                                  "_blank", 0,
+                                                  [])
+        except:
+            raise RuntimeWarning("Creating a new doc didn't work.")
+        return newdoc
 
-
-def create_new_file(desktop):
-    try:
-        #https://wiki.openoffice.org/wiki/Writer/API/Overview#Creating.2C_opening_a_Writer_Document
-        newdoc = desktop.loadComponentFromURL("private:factory/swriter", "_blank", 0,
-                                     [])
-    except:
-        raise RuntimeWarning("Creating a new doc didn't work.")
-    return newdoc
 
 def create_a_table(file, rows, columns):
     table_string = "com.sun.star.text.TextTable"  # https://wiki.openoffice.org/wiki/Writer/API/Tables
@@ -62,14 +67,15 @@ def part1():
     text = model.Text
     cursor = text.createTextCursor()
 
-    text.insertString( cursor, "Bambambooyah", 0 )
+    text.insertString( cursor, "This is string one\n", 0 )
     another_cursor = model.getCurrentController().getViewCursor()
-    text.insertString( another_cursor, "Howdy Woild", 0 )
+    text.insertString( another_cursor, "Esto es string dos\n", 0 )
 
 
 def main():
-    setup = set_up()
-    new_file = create_new_file(setup['desktop'])
+    creator=ResumeCreator()
+    setup = creator.do_set_up()
+    new_file = creator.create_new_file()
     content = [7, 3, 5]
     add_content_to_file(content, new_file)
 
